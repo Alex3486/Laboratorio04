c1b09aea0a71d59fb5c8b6c4bda26d90a7a2fd16
Edgar Pinto <mario.alex343@gmail.com>
2023-03-18 09:52:14 -0600

    Resolver ejercicio 1

--- Laboratorio04/Program.cs
+++ Laboratorio04/Program.cs
@@ -4,9 +4,9 @@
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Nombre: ");
-            Console.WriteLine("Carnet: ");
-            Console.WriteLine("Seccion: ");
+            Console.WriteLine("Nombre: Edgar Alexander Pinto Tobar");
+            Console.WriteLine("Carnet: 1162420");
+            Console.WriteLine("Seccion: 10");
         }
     }
 }
